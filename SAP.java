340ce185bb3614a94081c7d9228b8a67b1408540
nokkvikarlsson <nokkvi17@ru.is>
2018-10-19 11:17:01 +0000

    Added test in SAP constructor that check if the graph is acyclic and rooted

--- SAP.java
+++ SAP.java
@@ -7,19 +7,38 @@ import edu.princeton.cs.algs4.DirectedCycle;
 public class SAP {
 	
 	private Digraph digraph;
-	private BreadthFirstDirectedPaths BFD;
+	private BreadthFirstDirectedPaths BFD_V;
+	private BreadthFirstDirectedPaths BFD_W;
+	
 	
 	// constructor takes a digraph ( not necessarily a DAG )
 	public SAP ( Digraph G) {
 		
+		//Copy the digraph G to our digraph variable.
 		digraph = new Digraph(G);
 		
 		//Create a DirectedCycle data type to and copy digraph G into it so we can check if the graph is a DAG.
 		DirectedCycle cycle = new DirectedCycle(G);
 		
 		if(cycle.hasCycle()){
-			throw new IllegalArgumentException("The graph is not DAG");
+			throw new IllegalArgumentException("Graph is not acyclic");
 		}
+		
+		//Keep count of number of vertexes that have no directed path to another vertex.
+		int rootCounter = 0;
+		
+		//Loops over every vertex and checks if any vertex has no directed path to another.
+		for(int i = 0; i < digraph.V(); i++) {
+			if(!digraph.adj(i).iterator().hasNext()) {
+				rootCounter++; //If a vertex has no directed path it's a root.
+			}
+		}
+		
+		//If root counter isn't exactly one the graph is not rooted.
+		if(rootCounter < 1 && rootCounter > 1) {
+			throw new IllegalArgumentException("Graph is not rooted");
+		}
+		
 	}
 	
 	
@@ -49,6 +68,9 @@ public class SAP {
 	
 	// do unit testing of this class
 	public static void main ( String [] args ) {
+		
+		
+		
 		
 	}
 }
