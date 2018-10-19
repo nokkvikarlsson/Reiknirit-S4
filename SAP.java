7efc60f10812ef71a8638d20f17eb78daa9d5886
nokkvikarlsson <nokkvi17@ru.is>
2018-10-19 09:28:12 +0000

    Added empty functions with comments to SAP.java

--- SAP.java
+++ SAP.java
@@ -1,5 +1,43 @@
 package S4;
 
-public class SAP {
+import edu.princeton.cs.algs4.Digraph;
 
+public class SAP {
+	
+	Digraph graph;
+	
+	// constructor takes a digraph ( not necessarily a DAG )
+	public SAP ( Digraph G) {
+		
+	}
+	
+	
+	// length of shortest ancestral path between v and w; -1 if no such path
+	public int length ( int v , int w) {
+		
+		return 1;
+	}
+	
+	// a shortest common common ancestor of v and w; -1 if no such path
+	public int ancestor ( int v , int w) {
+		
+		return 1;
+	}
+	
+	// length of shortest ancestral path of vertex subsets A and B ; -1 if no such path
+	public int length ( Iterable < Integer > A , Iterable < Integer > B) {
+		
+		return 1;
+	}
+	
+	// a shortest common ancestor of vertex subsets A and B; -1 if no such path
+	public int ancestor ( Iterable < Integer > A , Iterable < Integer > B) {
+		
+		return 1;
+	}
+	
+	// do unit testing of this class
+	public static void main ( String [] args ) {
+		
+	}
 }
