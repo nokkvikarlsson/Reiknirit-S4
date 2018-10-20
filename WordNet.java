410a735973f428b73b3b561bb01a071cc056f1d9
Atli Gislason <atlig17@ru.is>
2018-10-20 12:46:50 +0000

    created wordnet api

---
+++ WordNet.java
@@ -0,0 +1,39 @@
+package s4;
+
+public class WordNet {
+
+	//Constructor takes the names of the two input files
+	public WordNet(String synsets, String hypernyms) {
+		
+	}
+	
+	//Returns all WordNet nouns
+	public Iterable<String> nouns(){
+	
+		return null;
+	}
+	
+	//Is the word a WordNet noun?
+	public boolean isNoun(String word) {
+		
+		return false;
+	}
+	
+	//Distance between Noun A and Noun B (defined below)
+	public int distance(String nounA, String nounB) {
+		
+		return -1;
+	}
+	
+	//A synset (second field of synsets.txt) that is the shortest common ancestor
+	//of Noun A and Noun B
+	public String SAP(String nounA, String nounB) {
+	
+		return null;
+	}
+	
+	//Do unit testing of this class
+	public static void main(String[] args) {
+		
+	}
+}
